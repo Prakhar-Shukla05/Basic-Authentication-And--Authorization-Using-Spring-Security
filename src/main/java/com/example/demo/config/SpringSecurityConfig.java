4ea2d6507e11b46b1f232994219e1c0318c74ab9
Prakhar-Shukla05 <prakhar3109@gmail.com>
2021-07-15 18:28:01 +0530

    Final Project Commit

---
+++ src/main/java/com/example/demo/config/SpringSecurityConfig.java
@@ -0,0 +1,49 @@
+package com.example.demo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+
+@Configuration
+public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Override
+	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+		// TODO Auto-generated method stub
+		auth.inMemoryAuthentication().withUser("Prakhar Shukla").password("12345678pwd")
+		.roles("ADMIN");
+	}
+	
+	//Security for all API
+	
+	/*
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.csrf().disable();
+		http.authorizeRequests()
+		.anyRequest().fullyAuthenticated().and().httpBasic();
+	}
+	*/
+	
+	//Security Based on URL
+	
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		// TODO Auto-generated method stub
+		http.csrf().disable();
+		http.authorizeRequests()
+		.antMatchers("/rest/**")
+		.fullyAuthenticated()
+		.and().httpBasic();
+	}
+	
+	
+	@Bean
+	public static NoOpPasswordEncoder passwordEncoder() {
+		return (NoOpPasswordEncoder) NoOpPasswordEncoder.getInstance();
+	}
+}
